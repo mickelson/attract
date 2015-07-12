f7dfe7ebc4fa87297b8713d29cf111bf98ac0449
Jeffrey Clark <dude@zaplabs.com>
2015-07-12 13:55:15 -0500

    Fix x86_64 compilation with MingW

--- extlibs/miniz/miniz.c
+++ extlibs/miniz/miniz.c
@@ -2848,7 +2848,7 @@ void *tdefl_write_image_to_png_file_in_memory(const void *pImage, int w, int h, 
   #include <stdio.h>
   #include <sys/stat.h>
 
-  #if defined(_MSC_VER) || defined(__MINGW64__)
+  #if (defined(_MSC_VER) && (defined(_WIN32) || defined(_WIN64)))
     static FILE *mz_fopen(const char *pFilename, const char *pMode)
     {
       FILE* pFile = NULL;
