ef53e5aa0bd36902810b2570fc7e0aeba621eebe
Andrew Mickelson <andrew.mickelson@gmail.com>
2015-09-06 13:48:02 -0700

    Fix font loading break from last commit

--- src/fe_overlay.cpp
+++ src/fe_overlay.cpp
@@ -381,13 +381,13 @@ int FeOverlay::languages_dialog()
 
 	if ( sel >= 0 )
 	{
-		std::string temp;
+		std::string temp1,temp2;
 		m_feSettings.set_language( ll[sel].language );
 
 		for ( std::vector<std::string>::iterator itr=ll[sel].font.begin();
 				itr != ll[sel].font.end(); ++itr )
 		{
-			if ( m_feSettings.get_font_file( temp, *itr ) )
+			if ( m_feSettings.get_font_file( temp1, temp2, *itr ) )
 			{
 				m_feSettings.set_info( FeSettings::DefaultFont,
 					*itr );
