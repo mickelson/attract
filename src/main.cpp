0ce2dad34eb4bdc865c9cfb91529b57630a0b806
Benjamin Hodgetts <ben@xnode.org>
2017-06-25 17:28:30 +0100

    Save location in the playlist when launching a game
    That way if AttractMode crashes or is killed, we'll retain our previous locations in the lists.

--- src/main.cpp
+++ src/main.cpp
@@ -206,6 +206,7 @@ int main(int argc, char *argv[])
 		else if (( launch_game )
 			&& ( !soundsys.is_sound_event_playing( FeInputMap::Select ) ))
 		{
+			feSettings.save_state();
 			const std::string &emu_name = feSettings.get_rom_info( 0, 0, FeRomInfo::Emulator );
 			if ( emu_name.compare( 0, 1, "@" ) == 0 )
 			{
