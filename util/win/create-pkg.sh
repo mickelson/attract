1b7f9b6c0e00a79b26f354903149a0b52a173800
Andrew Mickelson <andrew.mickelson@gmail.com>
2018-07-11 20:46:38 -0700

    [windows] combine attract.exe and attract-console.exe into the same package
    - get rid of the different console and no console packages on windows.  The
      32-bit and 64-bit windows packaged now include both executables

--- util/win/create-pkg.sh
+++ util/win/create-pkg.sh
@@ -70,6 +70,11 @@ LLIMIT=$(awk 'BEGIN{printf"%.1f",'${NPROC}'/2}')
 make -C .. clean
 eval make -C .. -j${NPROC} -l${LLIMIT} ${MAKEOPTS} TOOLCHAIN=i686-w64-mingw32.static $@
 mv ../attract.exe "${APP}"/attract.exe
+
+make -C .. clean
+eval make -C .. -j${NPROC} -l${LLIMIT} ${MAKEOPTS} WINDOWS_CONSOLE=1 TOOLCHAIN=i686-w64-mingw32.static $@
+mv ../attract.exe "${APP}"/attract-console.exe
+
 pushd "${APP}" >/dev/null
 zip -r ../attract-${VERSION}-win32.zip *
 popd >/dev/null
@@ -78,24 +83,13 @@ popd >/dev/null
 make -C .. clean
 eval make -C .. -j${NPROC} -l${LLIMIT} ${MAKEOPTS} TOOLCHAIN=x86_64-w64-mingw32.static $@
 mv ../attract.exe "${APP}"/attract.exe
-pushd "${APP}" >/dev/null
-zip -r ../attract-${VERSION}-win64.zip *
-popd >/dev/null
 
-# 32-bit console
-make -C .. clean
-eval make -C .. -j${NPROC} -l${LLIMIT} ${MAKEOPTS} WINDOWS_CONSOLE=1 TOOLCHAIN=i686-w64-mingw32.static $@
-mv ../attract.exe "${APP}"/attract.exe
-pushd "${APP}" >/dev/null
-zip -r ../attract-${VERSION}-win32-console.zip *
-popd >/dev/null
-
-# 64-bit console
 make -C .. clean
 eval make -C .. -j${NPROC} -l${LLIMIT} ${MAKEOPTS} WINDOWS_CONSOLE=1 TOOLCHAIN=x86_64-w64-mingw32.static $@
-mv ../attract.exe "${APP}"/attract.exe
+mv ../attract.exe "${APP}"/attract-console.exe
+
 pushd "${APP}" >/dev/null
-zip -r ../attract-${VERSION}-win64-console.zip *
+zip -r ../attract-${VERSION}-win64.zip *
 popd >/dev/null
 
 mv "${SCRATCH}"/*.zip ..
