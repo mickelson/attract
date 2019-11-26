mickelson/attract

commit 7dc5c88ca15d756f53ff28e427f8b6be019a1db7

Issue #566 - fix filter info update when display changes
- When navigating displays that have the same layout, the filter information
  between each display was not being updated correctly
- We now reload the filter array in squirrel whenever the display changes

src/fe_vm.cpp
@@ -482,6 +482,31 @@ void FeVM::vm_init()
 	Sqrat::DefaultVM::Set( vm );
 }
 
+void FeVM::update_to_new_list( int var, bool reset_display )
+{
+	if ( reset_display )
+	{
+		//
+		// Populate script arrays that may change from display to display (currently just fe.filters)
+		//
+		Sqrat::Table fe( Sqrat::RootTable().GetSlot( _SC("fe") ) );
+
+		Sqrat::Table ftab;  // hack Table to Array because creating the Array straight up doesn't work
+		fe.Bind( _SC("filters"), ftab );
+		Sqrat::Array farray( ftab.GetObject() );
+
+		farray.Resize( 0 );
+
+		FeDisplayInfo *di = m_feSettings->get_display( m_feSettings->get_current_display_index() );
+		if ( di )
+		{
+			for ( int i=0; i < di->get_filter_count(); i++ )
+				farray.SetInstance( farray.GetSize(), di->get_filter( i ) );
+		}
+	}
+
+	FePresent::update_to_new_list( var, reset_display );
+}
 
 bool FeVM::on_new_layout()
 {
@@ -938,20 +963,12 @@ bool FeVM::on_new_layout()
 			m_feSettings->get_display( i ) );
 
 	//
-	// fe.filters
+	// Note the fe.filters array gets populated in call to FeVM::update_to_new_list(), since it
+	// gets reset even when the layout itself isn't necessarily reloaded (i.e. when navigating between
+	// displays that use the same layout)
 	//
-	FeDisplayInfo *di = m_feSettings->get_display( m_feSettings->get_current_display_index() );
-
-
 	Table ftab;  // hack Table to Array because creating the Array straight up doesn't work
 	fe.Bind( _SC("filters"), ftab );
-	Array farray( ftab.GetObject() );
-
-	if ( di )
-	{
-		for ( int i=0; i < di->get_filter_count(); i++ )
-			farray.SetInstance( farray.GetSize(), di->get_filter( i ) );
-	}
 
 	//
 	// fe.monitors

src/fe_present.cpp
@@ -1086,9 +1086,9 @@ void FePresent::load_layout( bool initial_load )
 	update_to_new_list( FromToNoValue, true );
 }
 
-void FePresent::update_to_new_list( int var, bool new_layout )
+void FePresent::update_to_new_list( int var, bool reset_display )
 {
-	update( true, new_layout );
+	update( true, reset_display );
 	on_transition( ToNewList, var );
 }
 
@@ -1289,7 +1289,7 @@ void FePresent::set_transforms()
 
 			float adjust_x = std::fabs( m_layoutSize.y * m_layoutScale.x - m_mon[0].size.x ) / 2;
 			float adjust_y = std::fabs( m_layoutSize.x * m_layoutScale.y - m_mon[0].size.y ) / 2;
-		
+
 			if ( actualRotation == FeSettings::RotateRight )
 			{
 				m_transform.translate( m_mon[0].size.x - adjust_x, adjust_y );

src/fe_present.hpp
@@ -204,7 +204,7 @@ public:
 	void load_screensaver();
 	void load_layout( bool initial_load=false );
 
-	void update_to_new_list( int var=0, bool new_layout=false );
+	virtual void update_to_new_list( int var=0, bool reset_display=false ); // NOTE virtual function!
 	void on_end_navigation();
 	void redraw_surfaces();
 

src/fe_vm.hpp
@@ -120,6 +120,8 @@ public:
 	bool poll_command( FeInputMap::Command &c, sf::Event &ev, bool &from_ui );
 	void clear(); // override of base class clear()
 
+	void update_to_new_list( int var=0, bool reset_display=false ); // NOTE: override virtual function from FePresent
+
 	// runs .attract/emulators/template/setup.nut to generate default emulator
 	// configs and detect emulators.  Prompts user to automaticallly import emulators
 	//

commit 2443c9655bc0f389642216f19a054cbbee13db68

Issue #566 - Fixed regression caused by https://github.com/mickelson/attract/commit/7dc5c88ca15d756f53ff28e427f8b6be019a1db7

src/fe_vm.cpp
@@ -966,12 +966,21 @@ bool FeVM::on_new_layout()
 			m_feSettings->get_display( i ) );
 
 	//
-	// Note the fe.filters array gets populated in call to FeVM::update_to_new_list(), since it
+	// Note the fe.filters array also gets repopulated in call to FeVM::update_to_new_list(), since it
 	// gets reset even when the layout itself isn't necessarily reloaded (i.e. when navigating between
 	// displays that use the same layout)
 	//
 	Table ftab;  // hack Table to Array because creating the Array straight up doesn't work
 	fe.Bind( _SC("filters"), ftab );
+
+	FeDisplayInfo *di = m_feSettings->get_display( m_feSettings->get_current_display_index() );
+	if ( di )
+	{
+		Array farray( ftab.GetObject() );
+
+		for ( i=0; i < di->get_filter_count(); i++ )
+			farray.SetInstance( farray.GetSize(), di->get_filter( i ) );
+	}
 
 	//
 	// fe.monitors